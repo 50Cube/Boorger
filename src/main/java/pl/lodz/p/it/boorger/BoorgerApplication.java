283b0467240211a802d4fe619d4d390e61ebe3de
Jakub Bogdan <kubab1998@o2.pl>
2020-07-20 22:27:22 +0200

    Spring project created

---
+++ src/main/java/pl/lodz/p/it/boorger/BoorgerApplication.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.it.boorger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoorgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoorgerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/lodz/p/it/boorger/BoorgerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.it.boorger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoorgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
