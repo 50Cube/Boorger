89d6e92d5218da461d50fac8a5a46ae15ad5c6d3
Jakub Bogdan <kubab1998@o2.pl>
2020-07-21 20:37:20 +0200

    Travis bugfix

---
+++ src/main/front-end/build/precache-manifest.5af450e0204d2339af1e18e1be1580c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7003a55692e3914fc2a2be099cffd6cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f699fac14664f73e9d64",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "87ba4f6953e3f660c876",
+    "url": "/static/js/2.b9fc0f63.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b9fc0f63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f699fac14664f73e9d64",
+    "url": "/static/js/main.6d300ada.chunk.js"
+  },
+  {
+    "revision": "b757cbfa5b52942fc711",
+    "url": "/static/js/runtime-main.e327d292.js"
+  }
+]);
