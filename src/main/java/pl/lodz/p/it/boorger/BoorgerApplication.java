8b26a8570a88743c80afb0b3dcd5e868d7467052
Jakub Bogdan <kubab1998@o2.pl>
2020-08-11 23:10:17 +0200

    EmailService

--- src/main/java/pl/lodz/p/it/boorger/BoorgerApplication.java
+++ src/main/java/pl/lodz/p/it/boorger/BoorgerApplication.java
@@ -2,7 +2,9 @@ package pl.lodz.p.it.boorger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class BoorgerApplication {
 
---
+++ src/main/java/pl/lodz/p/it/boorger/utils/EmailService.java
@@ -0,0 +1,29 @@
+package pl.lodz.p.it.boorger.utils;
+
+import lombok.AllArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Component
+@AllArgsConstructor
+public class EmailService {
+
+    private JavaMailSender javaMailSender;
+
+    @Async
+    public void sendConfirmationEmail(String mail, String language, String token) throws MessagingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setTo(mail);
+        helper.setSubject(MessageProvider.getTranslatedText("email.confirmation.subject", language));
+        String link = "";
+        helper.setText("<a href=\"" + link + token + "\">" + MessageProvider.getTranslatedText("email.confirmation.body", language)
+                + "</a></br></br>" + MessageProvider.getTranslatedText("email.footer", language), true);
+        javaMailSender.send(message);
+    }
+}
