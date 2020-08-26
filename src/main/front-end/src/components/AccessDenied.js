40e8252b9b82e70a0750905c6b7091558eafc20d
Jakub Bogdan <kubab1998@o2.pl>
2020-08-26 23:15:30 +0200

    One type of error pages

--- src/main/front-end/src/components/AccessDenied.js
+++ src/main/front-end/src/components/AccessDenied.js
@@ -1,44 +1,18 @@
 import React, { Component } from "react";
 import Translate from '../i18n/Translate';
-import '../css/AccessDenied.css';
+import ErrorPageImage from "./ErrorPageImage";
+import '../css/ErrorPageImage.css';
 
 export default class AccessDenied extends Component {
 
     render() {
         return (
-            <div className="div403">
-                <div className="maincontainer">
-                    <div className="bat">
-                        <img className="wing leftwing"
-                             src="https://www.blissfullemon.com/wp-content/uploads/2018/09/bat-wing.png" alt="loading"/>
-                            <img className="body"
-                                 src="https://www.blissfullemon.com/wp-content/uploads/2018/09/bat-body.png" alt="bat"/>
-                                <img className="wing rightwing"
-                            src="https://www.blissfullemon.com/wp-content/uploads/2018/09/bat-wing.png" alt="loading" />
-                    </div>
-                    <div className="bat">
-                        <img className="wing leftwing"
-                             src="https://www.blissfullemon.com/wp-content/uploads/2018/09/bat-wing.png" alt="loading"/>
-                            <img className="body"
-                                 src="https://www.blissfullemon.com/wp-content/uploads/2018/09/bat-body.png" alt="bat"/>
-                                <img className="wing rightwing"
-                                     src="https://www.blissfullemon.com/wp-content/uploads/2018/09/bat-wing.png" alt="loading"/>
-                    </div>
-                    <div className="bat">
-                        <img className="wing leftwing"
-                             src="https://www.blissfullemon.com/wp-content/uploads/2018/09/bat-wing.png" alt="loading"/>
-                            <img className="body"
-                                 src="https://www.blissfullemon.com/wp-content/uploads/2018/09/bat-body.png" alt="bat"/>
-                                <img className="wing rightwing"
-                                     src="https://www.blissfullemon.com/wp-content/uploads/2018/09/bat-wing.png" alt="loading"/>
-                    </div>
-                    <img className="foregroundimg"
-                         src="https://www.blissfullemon.com/wp-content/uploads/2018/09/HauntedHouseForeground.png"
-                         alt="haunted house"/>
-
+            <div className="errorDiv">
+                <ErrorPageImage/>
+                <div className="message-box">
+                    <h1>ERROR 403</h1>
+                    <h2>{Translate('accessDenied')}</h2>
                 </div>
-                <h1 className="errorcode">ERROR 403</h1>
-                <div className="errortext">{Translate('accessDenied')}</div>
             </div>
         )
     }
